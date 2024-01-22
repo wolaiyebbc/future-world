a7aea425b6f7ce92ccf57b3ac21cb441b708bdac
wolaiyebbc <126575133+wolaiyebbc@users.noreply.github.com>
2024-01-22 10:09:05 +0800

    试验

---
+++ 编程练习（4.8.1）.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void)
+{
+	char mingzi[40];
+	char xingshi[40];
+	
+	printf("enter your first name:\n");
+	scanf("%s",mingzi);  //输入这里不能够加\n换行； 
+	printf("enter your last name:\n");
+	scanf("%s",xingshi);  //输入这里不能够加\n换行；
+	
+	printf("%s,%s\n",mingzi,xingshi);
+	
+	return 0;
+} 
